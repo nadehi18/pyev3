e5c646bc42f83f5e476ca8d53aa87fa9d91cfd8e
Nathan Higley <nadehi1824@gmail.com>
2016-11-09 16:37:39 -0500

    Changed spin.

--- spin.py
+++ spin.py
@@ -1,19 +1,19 @@
 import ev3dev.ev3 as ev3
 from time import sleep
 
-
-
 motor_b = ev3.LargeMotor(address='outB')
 motor_c = ev3.LargeMotor(address='outC')
 
 motor_b.command = 'run-direct'
 motor_c.command = 'run-direct'
 
-motor_b.duty_cycle_sp = 50
-motor_c.duty_cycle_sp = -50
+while True:
 
-sleep(10)
+    motor_b.duty_cycle_sp = 50
+    motor_c.duty_cycle_sp = -50
 
-motor_b.duty_cycle_sp = 0
-motor_c.duty_cycle_sp = 0
+    sleep(.1)
+
+    motor_b.duty_cycle_sp = 0
+    motor_c.duty_cycle_sp = 0
 
