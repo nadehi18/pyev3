acfdce6bb3659603095a27c1b079e832ae91193f
Nathan Higley <nadehi1824@gmail.com>
2016-11-09 16:43:55 -0500

    Changed spin.

--- spin.py
+++ spin.py
@@ -7,14 +7,14 @@ motor_c = ev3.LargeMotor(address='outC')
 motor_b.command = 'run-direct'
 motor_c.command = 'run-direct'
 
-while True:
+#while True:
 
-    motor_b.duty_cycle_sp = 100
-    motor_c.duty_cycle_sp = -100
+motor_b.duty_cycle_sp = 100
+motor_c.duty_cycle_sp = -100
 
-    sleep(.1)
+    #sleep(.1)
 
-    motor_b.duty_cycle_sp = 0
-    motor_c.duty_cycle_sp = 0
+    #motor_b.duty_cycle_sp = 0
+    #motor_c.duty_cycle_sp = 0
 
-    sleep(.5)
+    #sleep(.5)
